8b32675ee75f5e86fa8a029bde5495aa581ebf36
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-08 13:23:51 +0530

    add Db call

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-
+const DbConnect = require("./databse");
 const express = require("express");
-
 const app = express();
+const router = require("./routes");
 
 const PORT = process.env.PORT || 5500;
-
+DbConnect();
+app.use(express.json());
+app.use(router);
 app.get("/", (req, res) => {
   res.send("Hello from express js");
 });
